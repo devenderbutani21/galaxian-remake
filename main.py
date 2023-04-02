de9c6f7c93bad88e144afd49c5e1ceae52d1908b
devenderbutani21 <devenderbutani21@gmail.com>
2023-04-02 11:23:12 -0400

    Music and Laser working

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import sys
 import os
 import pygame as pg
+from pygame.locals import *
 
 pg.init() # initialize pygame
+
+pg.mixer.init() # initialize mixer for music
+
 
 class ScrollingBackground:
     def __init__(self, screenheight, imagefile):
@@ -36,6 +40,35 @@ class HeroShip:
     def UpdateCoords(self,x):
         self.left = x - self.shape.get_width()/2
 
+class Laser(pg.sprite.Sprite):
+    def __init__(self, imagefile, x, y):
+        super().__init__()
+        self.image = pg.image.load(imagefile)
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+        self.speed_y = 800 # adjust laser speed as needed
+
+    def UpdateCoords(self, time):
+        distance_y = self.speed_y * time
+        self.rect.y -= distance_y
+        if self.rect.bottom < 0:
+            self.kill()
+
+    def Show(self, surface):
+        surface.blit(self.image, self.rect)
+
+class LaserGroup(pg.sprite.Group):
+    def __init__(self):
+        super().__init__()
+
+    def UpdateCoords(self, time):
+        for laser in self.sprites():
+            laser.UpdateCoords(time)
+
+    def Show(self, surface):
+        for laser in self.sprites():
+            laser.Show(surface)
+
 clock = pg.time.Clock()
 
 screenwidth, screenheight = (480, 600)
@@ -52,6 +85,10 @@ bg_speed = 100
 # Load the background image here. Make sure the file exists!
 # bg = pg.image.load(os.path.join("./", "background.jpg"))
 
+pg.mixer.music.load('space.mp3')
+
+pg.mixer.music.play(-1)
+
 StarField = ScrollingBackground(screenheight, "background.jpg")
 
 pg.mouse.set_visible(0)
@@ -59,6 +96,8 @@ pg.mouse.set_visible(0)
 pg.display.set_caption('Galaxian Remake')
 
 Hero = HeroShip(screenheight, screenwidth, "ship.png")
+
+laser_group = LaserGroup()
 
 while True:
     time = clock.tick(framerate)/1000.0
@@ -68,12 +107,15 @@ while True:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             sys.exit()
+        if event.type == pg.MOUSEBUTTONDOWN:
+            if event.button == 1: # left mouse button
+                # Create laser sprite and add to sprite group
+                laser = Laser("laser.png", Hero.left + Hero.shape.get_width() / 2, Hero.top)
+                laser_group.add(laser)
 
     StarField.UpdateCoords(bg_speed, time)
-
-    StarField.Show(screen)
-
     Hero.UpdateCoords(x)
-
+    StarField.Show(screen)
     Hero.Show(screen)
+    laser_group.Show(screen)
     pg.display.update()
